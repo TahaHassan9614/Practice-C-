ae55f7ed0aa43f5f410a4e0eeee5cea63632dc06
TahaHassan9614 <tahasmarti9614@gmail.com>
2017-05-16 21:35:27 +0500

    Add files via upload

---
+++ nested structure.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+struct movie
+{
+	string title;
+	int year;
+	float rating;
+	int year1;
+	
+};
+
+struct friends
+{
+	string name;
+	string fv_movie;
+	string email;
+	
+	struct movie Add;
+};
+
+int main()
+{
+	int i;
+	friends f;
+	
+	cout<<"Enter friend name"<<endl;
+	cin>>f.name;
+	
+	cout<<"Enter movie"<<endl;
+	cin>>f.Add.title;
+	
+	cout<<"Enter Year"<<endl;
+	cin>>f.Add.year1;
+		
+	cout<<"Enter favourite movie "<<endl;
+	cin>>f.fv_movie;
+	
+	cout<<"Enter IMDB rating"<<endl;
+	cin>>f.Add.rating;
+	
+	cout<<"Enter Year"<<endl;
+	cin>>f.Add.year;
+	
+	cout<<"Enter Email"<<endl;
+	cin>>f.email;
+	
+	
+	
+	cout<<f.name<<" watching "<<f.Add.title<<" release in "<<f.Add.year1<<" but he like "<<f.fv_movie<<" release in "<<f.Add.year<<" rating is "<<f.Add.rating<<" & the email is " <<f.email<<endl;     
+	
+	
+	
+	return 0;
+}
