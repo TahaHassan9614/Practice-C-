5fe6c3bd71326c3bd28c482d289578bb50e44884
TahaHassan9614 <tahasmarti9614@gmail.com>
2017-06-03 15:42:48 +0500

    Add files via upload

---
+++ Auto file create.cpp
@@ -0,0 +1,38 @@
+#include<fstream>
+#include<iostream>
+using namespace std;
+
+struct book
+{
+	
+	float price;
+	string title;
+	string author;
+};
+
+
+int main()
+{
+	book b[5];
+//	fstream outfile;
+	
+	cout<<"Enter book name, author and price"<<endl;
+	for(int i=0;i<=2;i++)
+	{
+		cin>>b[i].title>>b[i].author>>b[i].price;
+	}
+	
+	
+	std::ofstream outfile ("test.txt");
+	{
+	for(int i=0;i<=2;i++)
+	{
+		outfile<<b[i].title<<endl;
+		outfile<<b[i].author<<endl;
+		outfile<<b[i].price<<endl;
+	}
+	}
+	
+	outfile.close();
+	
+}
